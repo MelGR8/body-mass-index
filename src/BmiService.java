f25bea9fd78ed7595c8d95e1e461ed9e68b1df95
Nikita Milekhin <Milekhin7597@gmail.com>
2024-04-04 15:54:18 +0300

    BMI

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+    public int calculate(int weight, double height) {
+        int result;
+        {
+            result = (int) (weight / (height * height));
+
+            return result;
+        }
+    }
+}
